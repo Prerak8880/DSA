d2361f6eb216a6e1b7192f034ae8010eb90c04ae
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-09-18 23:08:37 +0530

    Update Activities_Selection.c

--- Greedy/Activities_Selection.c
+++ Greedy/Activities_Selection.c
@@ -31,3 +31,43 @@ int main() {
     cout << maxActivities(arr, n) << endl;
     return 0;
 }
+
+//when two different arrays are given 
+// As start time array -> s[] = {x1, x2, x3, ...}
+// As finish time array -> f[] = {y1, y2, y3, ...}
+
+#include <iostream>
+using namespace std;
+
+void printMaxActivities(int s[], int f[], int n) {
+    int i = 0;
+    cout << "Following activities are selected: " << i << " ";
+
+    for (int j = 1; j < n; j++) {
+        if (s[j] >= f[i]) {
+            cout << j << " ";
+            i = j;
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of activities: ";
+    cin >> n;
+
+    int s[n], f[n];
+    cout << "Enter the start times of the activities: ";
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+
+    cout << "Enter the finish times of the activities: ";
+    for (int i = 0; i < n; i++) {
+        cin >> f[i];
+    }
+
+    printMaxActivities(s, f, n);
+    return 0;
+}
