6aa6f3eae89d34b921cf2c0f882a011036b068c8
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-10-01 23:56:04 +0530

    Update Freq_Char.cpp

--- Hashing/Freq_Char.cpp
+++ Hashing/Freq_Char.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    string str;
+    cin >> str;
+    
+    map<char, int> mpp;
+    for(char c : str){
+        mpp[c]++;
+    }
+    
+    int q;
+    cin >> q;
+    
+    while(q--){
+        char num;
+        cin >> num;
+        
+        cout << mpp[num] << " ";
+        
+    }
+    
+    return 0;
+}
