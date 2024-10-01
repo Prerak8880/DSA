e061007d80cddbe1c116cf39827baa34cc282e19
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-10-02 04:33:38 +0530

    Create remove_spaces.cpp

---
+++ Strings/remove_spaces.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string rem_vowel(string str){
+    
+    regex r(" +");
+    
+    return regex_replace(str,r,"");
+}
+
+int main(){
+    string str;
+    getline(cin,str);
+    
+    cout << rem_vowel(str);
+    
+    return 0;
+}
