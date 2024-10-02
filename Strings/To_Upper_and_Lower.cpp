900a934f9b30afbd6f4f020dcb6cd7aae8836f01
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-10-03 01:40:08 +0530

    Create To_Upper_and_Lower.cpp

---
+++ Strings/To_Upper_and_Lower.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    cout << str << endl;
+    
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    cout << str;
+    
+    return 0;
+}
